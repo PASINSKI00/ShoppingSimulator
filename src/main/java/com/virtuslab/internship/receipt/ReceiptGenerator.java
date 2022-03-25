ce9a559da02feb00036dd38cdca9be008a801133
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-03-25 21:26:59 +0100

    Added missing functionality: Adding products with quantities from the basket to the List with conversion to ReceiptEntity

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,14 +1,30 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
         List<ReceiptEntry> receiptEntries = new ArrayList<>();
+
+        Map<Product,Integer> map = new HashMap<>();
+        for(Product product : basket.getProducts()){
+            if(!map.containsKey(product))
+                map.put(product, 1);
+            else
+                map.put(product, map.get(product) + 1);
+        }
+
+        for(Product product : map.keySet()){
+          receiptEntries.add(new ReceiptEntry(product, map.get(product)));
+        }
+
         return new Receipt(receiptEntries);
     }
 }
